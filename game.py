df5f93e7d020dbe2296d8718416c0e44d920223e
VladyslavStolbov <stolbovvladyslavcg@gmail.com>
2023-12-09 07:57:02 +0200

    OOP aproach implemented

--- game.py
+++ game.py
@@ -11,155 +11,153 @@ from pygame.locals import (
 
 from button import Button
 
-# Initialize pygame
-pygame.init()
 
-# Constants
-SCREEN_WIDTH = 320
-SCREEN_HEIGHT = 565
-FPS = 60
-GRID_SIZE = 3
-RECT_SIZE = 90
-SPACE_BETWEEN = 5
-HORIZONTAL_OFFSET = 5
-VERTICAL_OFFSET = 37.5
+class TicTacToe:
+    def __init__(self):
+        # Initialize pygame
+        pygame.init()
 
+        # Constants
+        self.SCREEN_WIDTH = 320
+        self.SCREEN_HEIGHT = 565
+        self.FPS = 60
+        self.GRID_SIZE = 3
+        self.RECT_SIZE = 90
+        self.SPACE_BETWEEN = 5
+        self.HORIZONTAL_OFFSET = 5
+        self.VERTICAL_OFFSET = 37.5
+        self.TOTAL_WIDTH = self.GRID_SIZE * self.RECT_SIZE + (self.GRID_SIZE - 1) * self.SPACE_BETWEEN
+        self.TOTAL_HEIGHT = self.GRID_SIZE * self.RECT_SIZE + (self.GRID_SIZE - 1) * self.SPACE_BETWEEN
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-start_menu = pygame.image.load("assets/start_menu.PNG").convert()
-start_menu = pygame.transform.scale_by(start_menu, 5)
-grid = pygame.image.load("assets/grid.PNG").convert()
-grid = pygame.transform.scale_by(grid, 5)
-x_image = pygame.image.load("assets/x.PNG").convert_alpha()
-x_image = pygame.transform.scale_by(x_image, 5)
-o_image = pygame.image.load("assets/o.PNG").convert_alpha()
-o_image = pygame.transform.scale_by(o_image, 5)
-play_button_image = pygame.image.load("assets/play_button.PNG").convert_alpha()
-play_button_image = pygame.transform.scale_by(play_button_image, 5)
-quit_button_image = pygame.image.load("assets/quit_button.PNG").convert_alpha()
-quit_button_image = pygame.transform.scale_by(quit_button_image, 5)
-clock = pygame.time.Clock()
-turn = 'X'
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.start_menu = pygame.image.load("assets/start_menu.PNG").convert()
+        self.start_menu = pygame.transform.scale_by(self.start_menu, 5)
+        self.grid = pygame.image.load("assets/grid.PNG").convert()
+        self.grid = pygame.transform.scale_by(self.grid, 5)
+        self.x_image = pygame.image.load("assets/x.PNG").convert_alpha()
+        self.x_image = pygame.transform.scale_by(self.x_image, 5)
+        self.o_image = pygame.image.load("assets/o.PNG").convert_alpha()
+        self.o_image = pygame.transform.scale_by(self.o_image, 5)
+        self.play_button_image = pygame.image.load("assets/play_button.PNG").convert_alpha()
+        self.play_button_image = pygame.transform.scale_by(self.play_button_image, 5)
+        self.quit_button_image = pygame.image.load("assets/quit_button.PNG").convert_alpha()
+        self.quit_button_image = pygame.transform.scale_by(self.quit_button_image, 5)
+        self.clock = pygame.time.Clock()
+        self.turn = 'X'
 
-# Constants related to the game board
-TOTAL_WIDTH = GRID_SIZE * RECT_SIZE + (GRID_SIZE - 1) * SPACE_BETWEEN
-TOTAL_HEIGHT = GRID_SIZE * RECT_SIZE + (GRID_SIZE - 1) * SPACE_BETWEEN
-START_X = (screen.get_width() - TOTAL_WIDTH) // 2 + HORIZONTAL_OFFSET
-START_Y = (screen.get_height() - TOTAL_HEIGHT) // 2 + VERTICAL_OFFSET
+        self.START_X = (self.screen.get_width() - self.TOTAL_WIDTH) // 2 + self.HORIZONTAL_OFFSET
+        self.START_Y = (self.screen.get_height() - self.TOTAL_HEIGHT) // 2 + self.VERTICAL_OFFSET
 
-
-def create_board(start_x, start_y):
-    board = {}
-    for row in range(GRID_SIZE):
-        for column in range(GRID_SIZE):
-            x = start_x + column * (RECT_SIZE + SPACE_BETWEEN)
-            y = start_y + row * (RECT_SIZE + SPACE_BETWEEN)
-            board[(x, y)] = ""
-    return board
+        self.board = self.create_board()
 
 
-def switch_turn():
-    return "O" if turn == "X" else "X"
+    def create_board(self):
+        board = {}
+        for row in range(self.GRID_SIZE):
+            for column in range(self.GRID_SIZE):
+                x = self.START_X + column * (self.RECT_SIZE + self.SPACE_BETWEEN)
+                y = self.START_Y + row * (self.RECT_SIZE + self.SPACE_BETWEEN)
+                board[(x, y)] = ""
+        return board
 
+    def switch_turn(self):
+        return "O" if self.turn == "X" else "X"
 
-def check_game_state(board, start_x, start_y, turn):
-    lines = [
-        [(start_x + c * 95, start_y + r * 95) for c in range(3)] for r in range(3)
-    ] + [
-        [(start_x + c * 95, start_y + r * 95) for r in range(3)] for c in range(3)
-    ] + [
-        [(start_x + i * 95, start_y + i * 95) for i in range(3)],
-        [(start_x + i * 95, start_y + (2 - i) * 95) for i in range(3)],
-    ]
+    def check_game_state(self):
+        lines = [
+                    [(self.START_X + c * 95, self.START_Y + r * 95) for c in range(3)] for r in range(3)
+                ] + [
+                    [(self.START_X + c * 95, self.START_Y + r * 95) for r in range(3)] for c in range(3)
+                ] + [
+                    [(self.START_X + i * 95, self.START_Y + i * 95) for i in range(3)],
+                    [(self.START_X + i * 95, self.START_Y + (2 - i) * 95) for i in range(3)],
+                ]
 
-    for line in lines:
-        if all(board[pos] == turn for pos in line):
-            return "win"
+        for line in lines:
+            if all(self.board[pos] == self.turn for pos in line):
+                return "win"
 
-    if all(symbol != "" for symbol in board.values()):
-        return "draw"
+        if all(symbol != "" for symbol in self.board.values()):
+            return "draw"
 
-    return "ongoing"
+        return "ongoing"
 
+    def minimax(self):
+        pass
 
-def minimax(board, depth, maximizing_player):
-    pass
+    def find_best_move(self):
+        pass
 
+    def main_menu(self):
 
-def find_best_move(board):
-    pass
+        quit_button = Button(image=self.quit_button_image, position=(162, 337))
+        play_button = Button(image=self.play_button_image, position=(162, 287))
 
+        while True:
 
-def main_menu():
+            mouse_x, mouse_y = pygame.mouse.get_pos()
 
-    quit_button = Button(image=quit_button_image, position=(162, 337))
-    play_button = Button(image=play_button_image, position=(162, 287))
-
-    while True:
-
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-
-        screen.blit(start_menu, (0, 0))
-        quit_button.update(screen)
-        play_button.update(screen)
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if play_button.is_clicked((mouse_x, mouse_y)):
-                    play()
-                if quit_button.is_clicked((mouse_x, mouse_y)):
+            self.screen.blit(self.start_menu, (0, 0))
+            quit_button.update(self.screen)
+            play_button.update(self.screen)
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if play_button.is_clicked((mouse_x, mouse_y)):
+                        self.play()
+                    if quit_button.is_clicked((mouse_x, mouse_y)):
+                        pygame.quit()
+                        sys.exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+                elif event.type == QUIT:
                     pygame.quit()
                     sys.exit()
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
+
+            pygame.display.update()
+
+    def play(self):
+
+        self.screen.blit(self.grid, (0, 0))
+
+        while True:
+            # Input handler
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+                elif event.type == QUIT:
                     pygame.quit()
                     sys.exit()
-            elif event.type == QUIT:
-                pygame.quit()
-                sys.exit()
 
-        pygame.display.update()
+                elif event.type == MOUSEBUTTONDOWN:
+                    if event.button == BUTTON_LEFT:
+                        # Placing image and switch turn
+                        x, y = pygame.mouse.get_pos()
+                        for position, symbol in self.board.items():
+                            rect = pygame.Rect(position[0], position[1], self.RECT_SIZE, self.RECT_SIZE)
+                            if rect.collidepoint(x, y) and symbol == "":
+                                if self.turn == "X":
+                                    self.screen.blit(self.x_image, rect)
+                                    self.board[position] = "X"
+                                else:
+                                    self.screen.blit(self.o_image, rect)
+                                    self.board[position] = "O"
+                                # Check game state
+                                game_state = self.check_game_state()
+                                if game_state != "ongoing":
+                                    print(f"{self.turn} wins!" if game_state == "win" else "It's a draw!")
+                                    pygame.quit()
+                                    sys.exit()
+                                else:
+                                    self.turn = self.switch_turn()
+
+            pygame.display.flip()
+            self.clock.tick(self.FPS)
 
 
-board = create_board(START_X, START_Y)
-
-screen.blit(grid, (0, 0))
-
-
-def play():
-    while True:
-
-        # Input handler
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    running = False
-            elif event.type == QUIT:
-                running = False
-
-            elif event.type == MOUSEBUTTONDOWN:
-                if event.button == BUTTON_LEFT:
-                    # Placing image and switch turn
-                    x, y = pygame.mouse.get_pos()
-                    for position, symbol in board.items():
-                        rect = pygame.Rect(position[0], position[1], RECT_SIZE, RECT_SIZE)
-                        if rect.collidepoint(x, y) and symbol == "":
-                            if turn == "X":
-                                screen.blit(x_image, rect)
-                                board[position] = "X"
-                            else:
-                                screen.blit(o_image, rect)
-                                board[position] = "O"
-                            # Check game state
-                            game_state = check_game_state(board, START_X, START_Y, turn)
-                            if game_state != "ongoing":
-                                print(f"{turn} wins!" if game_state == "win" else "It's a draw!")
-                                running = False  # or take other actions based on game state
-                            else:
-                                turn = switch_turn()
-
-        pygame.display.flip()
-        clock.tick(FPS)
-
-    pygame.quit()
-
-main_menu()
+if __name__ == "__main__":
+    game = TicTacToe()
+    game.main_menu()
